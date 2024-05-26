c5cce710b9758e289fb3b26eeec7de326563de51
RyanButler53 <rbutler@g.hmc.edu>
2024-05-25 23:39:13 -0700

    Center image around origin

--- image.py
+++ image.py
@@ -3,11 +3,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import functools
 
 WINDOW_SCALE = 0.4
 
 def evalLine(slope, intercept, x):
     return slope * x + intercept
+
+def transpose(center_x, center_y, x,y):
+    return x-center_x, y-center_y
 
 
 points = []
@@ -25,10 +29,19 @@ points = [eval(p.strip()) for p in points[:-1]]
 x_s = [p[0] for p in points]
 y_s = [p[1] for p in points]
 
-plt.scatter(x_s, y_s, marker="D",color='fuchsia')
-
 # Center
-plt.scatter([np.average(x_s)], [np.average(y_s)], marker='D', color='midnightblue')
+center_x, center_y = (np.average(x_s), np.average(y_s))
+
+plt.scatter([0], [0], marker='D', color='midnightblue')
+
+transposex = functools.partial(transpose, center_x)
+transposePoints = functools.partial(transposex, center_y)
+
+transposedPts = list(map(transposePoints, [x_s], [y_s]))
+x_s = [p[0] for p in transposedPts]
+y_s = [p[1] for p in transposedPts]
+
+plt.scatter(x_s, y_s, marker="D",color='fuchsia')
 
 # Get window parameters and expand
 ax = plt.gca()
@@ -51,19 +64,27 @@ ax.set_ylim([y_min, y_max])
 for (slope, intercept) in all_lines:
     y1 = evalLine(slope, intercept, x_min)
     y2 = evalLine(slope, intercept, x_max)
-    plt.plot([x_min,x_max], [y1,y2],color = "black", linewidth=0.5)
+    x1,y1 = transposePoints(x_min, y1)
+    x2,y2 = transposePoints(x_max, y2)
+    plt.plot([x1,x2], [y1,y2],color = "black", linewidth=0.5)
 
 for i in range(len(points)-1):
     x,y = points[i]
     next_x,next_y = points[i+1]
+    x,y = transposePoints(x,y)
+    next_x,next_y = transposePoints(next_x, next_y)
     plt.plot([x,next_x], [y,next_y], "r--", linewidth=2)
 
 # Start and end segments. Run along the first topline from x_min to points[0]
 y_start = evalLine(toplines[0][0], toplines[0][1], x_min) 
-plt.plot([x_min,points[0][0]],[y_start, points[0][1]],color = "red", linewidth=2)
+x1,y1 = transposePoints(x_min,y_start)
+x2,y2 = transposePoints(points[0][0], points[0][1])
+plt.plot([x1,x2],[y1,y2],color = "red", linewidth=2)
 
 y_end = evalLine(toplines[-1][0], toplines[-1][1], x_max)
-plt.plot([points[-1][0], x_max], [points[-1][1], y_end], color="red", linewidth=2)
+x1,y1 = transposePoints(points[-1][0], points[-1][1])
+x2,y2 = transposePoints(x_max, y_end)
+plt.plot([x1,x2],[y1,y2],color = "red", linewidth=2)
 
 # plt.show()
 
