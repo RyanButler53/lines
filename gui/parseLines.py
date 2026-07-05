a798794c7a3dceae0d057404718640400fbda406
RyanButler53 <rmbutler@outlook.com>
2026-07-04 17:41:09 -0700

    Add lines as an entry point

--- gui/parseLines.py
+++ gui/parseLines.py
@@ -3,28 +3,6 @@ import py_lines as pl
 
 class InvalidLineException(Exception):
     """Raise an Invalid Line Exception"""
-
-# class Fraction():
-#     """Class to handle Fractions"""
-#     def __init__(self,num,den) -> None:
-#         assert(den)
-#         self.num = num
-#         self.den = den
-
-#     def __repr__(self) -> str:
-#         if (self.den == 1):
-#             return str(self.num)
-#         else:
-#             return f"{self.num}/{self.den}"
-    
-#     def reduce(self):
-#         gcd = math.gcd(self.num,self.den)
-#         self.num //= gcd
-#         self.den //= gcd
-#         return Fraction(self.num, self.den)
-
-#     def __eq__(self, other: object) -> bool:
-#         return self.num * other.den == self.den*other.num
     
 class LineParser():
 
